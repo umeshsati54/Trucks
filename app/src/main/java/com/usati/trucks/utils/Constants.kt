719e0a02fda169179f0bc43965797db23b3dba0f
Umesh Sati <umeshsati54@gmail.com>
2021-11-07 00:42:34 +0530

    Update Constants.kt

--- app/src/main/java/com/usati/trucks/utils/Constants.kt
+++ app/src/main/java/com/usati/trucks/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.usati.trucks.utils
 class Constants {
     companion object{
         const val BASE_URL = "https://api.mystral.in"
-        const val API_KEY = "g2qb5jvucg7j8skpu5q7ria0mu"
+        const val API_KEY = "ENTER API KEY"
     }
 }
